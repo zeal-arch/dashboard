420df8b7aa8c78c629b996e093a94e283033edc1
zealous sebastian <zealoussebastian464@gmail.com>
2026-08-21 12:06:07 +0530

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { inter, mulish, playfair } from "@/lib/fonts";
 import NextAuthProvider from "@/components/NextAuthProvider";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Personalized Content Dashboard",
@@ -33,7 +34,9 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </NextAuthProvider>
+        <Analytics />
       </body>
     </html>
   );
 }
+
